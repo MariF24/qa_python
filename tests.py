a096f3774b8f33d754958c0657ced31b79aacd6d
Maria Frolova <m89067536142@yandex.ru>
2024-04-21 23:53:09 +0300

    added test.ру

--- tests.py
+++ tests.py
@@ -1,3 +1,6 @@
+
+import pytest
+
 from main import BooksCollector
 
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
@@ -8,17 +11,64 @@ class TestBooksCollector:
     # обязательно указывать префикс test_
     # дальше идет название метода, который тестируем add_new_book_
     # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
+    def test_add_new_book_not_empty_dict_true(self):
+        books_collector = BooksCollector()
+        books_collector.add_new_book('Колобок')
+        assert 'Колобок' in books_collector.books_genre
 
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+    def test_set_book_genre_if_book_add_in_dict(self):
+        books_collector = BooksCollector()
+        books_collector.add_new_book('Шрек')
+        books_collector.set_book_genre('Шрек', 'Мультфильмы')
+        assert books_collector.books_genre['Шрек'] == 'Мультфильмы'
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+    def test_get_book_genre_if_genre_add(self):
+        books_collector = BooksCollector()
+        books_collector.add_new_book('Шрек')
+        books_collector.set_book_genre('Шрек', 'Мультфильмы')
+        assert books_collector.books_genre.get('Шрек')
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+
+    @pytest.mark.parametrize('genre', ['Фантастика', 'Ужасы'])
+    def test_get_books_with_specific_genre_input_two_genre(self, genre):
+        books_collector = BooksCollector()
+        books_collector.add_new_book('Люди в черном')
+        books_collector.set_book_genre('Люди в черном', 'Фантастика')
+        assert books_collector.get_books_with_specific_genre
+
+    def test_get_books_genre_if_dict_add(self):
+        books_collector = BooksCollector()
+        books_collector.add_new_book('Шрек')
+        books_collector.set_book_genre('Шрек', 'Мультфильмы')
+        assert books_collector.books_genre == {'Шрек':'Мультфильмы'}
+
+
+    def test_get_books_for_children_is_true(self):
+        books_collector = BooksCollector()
+        books_collector.add_new_book('Шрек')
+        books_collector.set_book_genre('Шрек', 'Мультфильмы')
+        assert books_collector.get_books_for_children
+
+    def test_add_book_in_favorites_if_add_in_books_genre(self):
+        books_collector = BooksCollector()
+        books_collector.add_new_book('Оно')
+        books_collector.add_book_in_favorites('Оно')
+        assert 'Оно' in books_collector.favorites
+
+    def test_delete_book_from_favorites_if_add_in_favorites(self):
+        books_collector = BooksCollector()
+        books_collector.add_new_book('Маша и медведь')
+        books_collector.add_book_in_favorites('Маша и медведь')
+        books_collector.delete_book_from_favorites('Маша и медведь')
+        assert 'Маша и медведь' not in books_collector.favorites
+
+
+
+    def test_get_list_of_favorites_books_if_book_add(self):
+        books_collector = BooksCollector()
+        books_collector.add_new_book('Маша и медведь')
+        books_collector.add_book_in_favorites('Маша и медведь')
+        assert books_collector.favorites == ['Маша и медведь']
+
+
+
